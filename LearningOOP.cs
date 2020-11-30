eb31224adac80712fa287fca9f6ea0606ca71321
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-30 10:02:43 +0200

    what is class definition, declaring its members

--- LearningOOP.cs
+++ LearningOOP.cs
@@ -3,14 +3,25 @@
 namespace _02.SimpleClassDefinition
 {
 
-    class Mammal
+    class Mammal   // Class definition consists of : keyword class->class declaration, 
+                   //inhereted class or implemeted interface, 
+                   //fields, constructors, properties, Methods, Eventes, inner types, etc...
     {
         public int age; //field variable
     }
 
-    class Person : Mammal  // class Person(Child class ) iherits class Mammal(Parent Class)
+    class Person : Mammal
+    // class Person(Child class ) iherits class Mammal(Parent Class)
     {
+        //class2 into class-> becasue we waht the class 2 to be accessible only into class
         private string name;  // field
+        private string age; //another field
+
+
+        public Person()  //empty Constructor
+        {
+
+        }
 
         public Person(string name)  //constructor, dont have return, dont have void type,  
         {
@@ -44,8 +55,10 @@ namespace _02.SimpleClassDefinition
         static void Main(string[] args)
         {
             Person p = new Person("Blagy");  //we create here the Instaance of teh class , the currnt Object "p".
+            Person p2 = new Person(); //creating new onject, this is valid  too //calls teh empty constuctor
             Console.WriteLine(p.Name);  //We access teh property  and print it 
-            p.Walk();   //we access the Method and perform call the method to do sth.//we call the method on p.
+            p.Walk();   //we access the Method and perform call the method to do sth.
+            Console.WriteLine(p2.Name);
 
         }
     }
